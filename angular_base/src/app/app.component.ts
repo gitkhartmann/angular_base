5c2775e355e25c891f1ff6caeb061810283ed4b6
Sergey <gitkhartmann.com>
2022-01-19 23:07:54 +0300

    добавил несколько компонентов

--- angular_base/src/app/app.component.ts
+++ angular_base/src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular_base';
+  title = 'Dinamic title';
+  number = new Date().getFullYear();
+  arr = [1, 2, 3, 4];
+  obj = { a: 1, b: { c: 3 } };
+  inputValue = '';
+
+  //img = 'https://w7.pngwing.com/pngs/452/495/png-transparent-react-javascript-angularjs-ionic-github-text-logo-symmetry-thumbnail.png';
+  constructor() {
+    // setTimeout(() => {
+    //   this.img = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg';
+    // }, 5000);
+    // }
+  }
+  onInput(event: any) {
+    this.inputValue = (<HTMLInputElement>event.target).value;
+  }
+  onClick() {
+    console.log('Кликнуто!');
+  }
+  onBlur(event: any) {
+    console.log('');
+  }
 }
