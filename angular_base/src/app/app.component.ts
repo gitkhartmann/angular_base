d1378ec3734d4809b5926ed5ff446ca385b6b538
Sergey <gitkhartmann.com>
2022-01-21 19:26:25 +0300

    закончил изучение блока Компоненты

--- angular_base/src/app/app.component.ts
+++ angular_base/src/app/app.component.ts
@@ -1,31 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Post {
+	title: string,
+	text: string,
+	id?: number
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Dinamic title';
-  number = new Date().getFullYear();
-  arr = [1, 2, 3, 4];
-  obj = { a: 1, b: { c: 3 } };
-  inputValue = '';
+export class AppComponent implements OnInit {
 
-  //img = 'https://w7.pngwing.com/pngs/452/495/png-transparent-react-javascript-angularjs-ionic-github-text-logo-symmetry-thumbnail.png';
-  constructor() {
-    // setTimeout(() => {
-    //   this.img = 'https://angular.io/assets/images/logos/angularjs/AngularJS-Shield.svg';
-    // }, 5000);
-    // }
-  }
-  onInput(event: any) {
-    this.inputValue = (<HTMLInputElement>event.target).value;
-  }
-  onClick() {
-    console.log('Кликнуто!');
-  }
-  onBlur(event: any) {
-    console.log('');
-  }
+	posts: Post[] = [
+		{ title: 'Изучение компонентов', text: 'Изучаю компоненты Andular', id: 1 },
+		//{ title: 'Второй заголовок', text: 'Второй текст', id: 2 }
+	]
+
+	updatePosts(post: Post) {
+		this.posts.unshift(post);
+	}
+	removePost(id: number) {
+		console.log(id);
+		this.posts = this.posts.filter(p => p.id !== id);
+	}
+	ngOnInit(): void {
+
+		//throw new Error('Method not implemented.');
+	}
+
 }
